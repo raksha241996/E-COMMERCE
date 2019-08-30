af62be01a950e4683e114757a0af41a687b2ea15
K Ramakrishna Vaidya <kgrvaidya@gmail.com>
2019-08-30 19:00:44 +0530

    Functionalities Added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,91 @@ import React, { Component } from 'react'
 import '../styles/componentStyles/Cart.scss'
 
 
-// const items = JSON.parse(localStorage.getItem('items'));
+// const CalculatePrice = () => {
+        
+// }
+
+
+let items = '';
 
 export default class Cart extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            items : []
+        };
+
+    }
+
+    componentDidMount(){
+        fetch('https://api.myjson.com/bins/ww7qv')   
+        .then(res => res.json())
+        .then(result => {this.setLocal(result.items)});
+        this.setState({items : this.getLocal()});   
+    }
+
+    componentWillUnmount() {
+        this.setLocal(this.state.items);
+    }
+
+    getLocal = () => JSON.parse(localStorage.getItem('items')) 
+    setLocal = (items) => {localStorage.setItem('items',JSON.stringify(items))}
+
+
+    increaseByOne = (id) => {
+        let items = this.state.items;
+        let ite = items.findIndex(x =>  x.id === id);
+        items[ite].quantity= parseInt(items[ite].quantity)+1;
+        this.setState({items : items});
+        this.setLocal(this.state.items);
+    }
+    
+    
+    decreaseByOne = (id) => {
+        let items = this.state.items;
+        let ite = items.findIndex(x =>  x.id === id);
+        
+        if(items[ite].quantity > 1){
+            items[ite].quantity= parseInt(items[ite].quantity)-1;  
+        }     
+        this.setState({items : items});
+        this.setLocal(this.state.items);
+    }
+
+    removeItem = (id) => {
+        let items = this.state.items;
+        items = items.filter(x => x.id !== id);
+        this.setState({items : items});
+        this.setLocal(this.state.items);
+        console.log(`Item with id ${id} Deleted`); 
+    }
+
+
+    
+
+    CreateListoOfItems = (props) => {
+        let itemList = [];
+
+        props.item.forEach((item) => {
+            itemList.push(
+                    <div id="item" key = {item.id}>
+                        <div id="imgndisnbtn">
+                            <img src={item.image} alt="item"/>
+                            <p>{item.disc}</p>
+                            <p>{item.quantity}</p>
+                            <button onClick ={() => {this.removeItem(item.id)}}>Remove</button>
+                        </div>
+                        <div id="btns">
+                            <button id="plus" onClick = {() => {this.increaseByOne(item.id)}}>+</button>
+                            <button id="minus" onClick ={() => {this.decreaseByOne(item.id)}}>-</button>
+                        </div>    
+                    </div>    
+            )
+        });
+
+        return itemList;
+
+    }
 
     
     render() {
@@ -12,38 +94,14 @@ export default class Cart extends Component {
             <div className="CartBody">                
                     <div id="itemListDisplay">
                         <div id="flexContainer">
-                            <div id="item">
-                                <div id="imgndisnbtn">
-                                    <img src="https://hackernoon.com/hn-images/1*To2H39eauxaeYxYMtV1afQ.png" alt="item"/>
-                                    <p>Hello</p>
-                                    <button>Remove</button>
-                                </div>
-                                <div id="btns">
-                                    <button id="plus">+</button>
-                                    <button id="minus">-</button>
-                                </div>    
-                            </div>
-                            <div id="item">
-                                <div id="imgndisnbtn">
-                                    <img src="https://hackernoon.com/hn-images/1*To2H39eauxaeYxYMtV1afQ.png" alt="item"/>
-                                    <p>DiscriptionDiscriptionDiscripti onDiscriptionDiscriptionDiscription</p>
-                                    <button>Remove</button>
-                                </div>
-                                <div id="btns">
-                                    <button id="plus">+</button>
-                                    <button id="minus">-</button>
-                                </div>    
-                            </div>
-                                                 
+                            <this.CreateListoOfItems item={this.state.items}/>                                                                                         
                         </div>
                     </div>
-    
                     <div id="priceDetails">
                         <p>Price Details</p>
-                    </div>
-                    
-                    
+                    </div>   
                 </div>
         )
     }
 }
+
